38f0a033b34bc8cc137e11f36cd172b56d6f7f7d
thirumanikumaran <thirumanikumaran20227306@gmail.com>
2026-02-19 16:59:09 +0530

    updated files

--- landproject/settings.py
+++ landproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r0p*x)%53z7n5rqp)xfnsuj2(vryxw%5r!o)x^5mg9nrit6=w(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
